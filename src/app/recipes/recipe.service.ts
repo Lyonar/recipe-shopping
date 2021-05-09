16bbc855bc844850c9b6ae678f543434e581d362
Lyonar <ff.derek@yahoo.com>
2021-05-09 14:33:49 -0700

    added http data-storage and recipes-resolver services

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,26 +8,33 @@ import { Recipe } from "./recipe.model";
 export class RecipeService {
     recipesChanged = new Subject<Recipe[]>();
 
-    private recipes: Recipe[] = [
-        new Recipe(
-            'A Test Recipe', 
-            'This is simply a test', 
-            'https://images.unsplash.com/photo-1534790566855-4cb788d389ec?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80',
-            [
-                new Ingredient('Meat', 4),
-                new Ingredient('French Fries', 2)
-            ]),
-        new Recipe(
-            'Another Test Recipe', 
-            'This is another test', 
-            'https://images.unsplash.com/photo-1458642849426-cfb724f15ef7?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1650&q=80',
-            [
-                new Ingredient('Pepperoni', 1),
-                new Ingredient('Olives', 2)
-            ])
-      ];
+    // private recipes: Recipe[] = [
+    //     new Recipe(
+    //         'A Test Recipe', 
+    //         'This is simply a test', 
+    //         'https://images.unsplash.com/photo-1534790566855-4cb788d389ec?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80',
+    //         [
+    //             new Ingredient('Meat', 4),
+    //             new Ingredient('French Fries', 2)
+    //         ]),
+    //     new Recipe(
+    //         'Another Test Recipe', 
+    //         'This is another test', 
+    //         'https://images.unsplash.com/photo-1458642849426-cfb724f15ef7?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1650&q=80',
+    //         [
+    //             new Ingredient('Pepperoni', 1),
+    //             new Ingredient('Olives', 2)
+    //         ])
+    //   ];
+
+    private recipes: Recipe[] = [];
 
     constructor(private slService: ShoppingListService) {}
+
+    setRecipes(recipes: Recipe[]) {
+        this.recipes = recipes;
+        this.recipesChanged.next(this.recipes.slice());
+    }
 
     getRecipes() {
         //this method returns a COPY of recipes
